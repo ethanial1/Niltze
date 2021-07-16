26e4ab4ab82e6af3a9ff6b2a929187a596962a2c
ethanial1 <camiloeiz00@gmail.com>
2021-07-16 14:29:52 -0500

    Inialización de la Cámara y el MobileNet

--- app.js
+++ app.js
@@ -6,3 +6,34 @@ let mobilenet;
 let model;
 let array = [];
 let isPredicting = false;
+
+// ========= Inialización de la Cámara y el MobileNet ============
+// ajustamos el tamaño de la camara
+function adjustVideoSize(width, height) {
+    const aspectRatio = width / height;
+    if(width >= height){
+        webcamElement.width = aspectRatio * webcamElement.height;
+    }else if(width < height){
+        webcamElement.height = webcamElement.width / aspectRatio;
+    }
+}
+
+async function setup() {
+    return new Promise((resolve, reject) => {
+        // permisos de camara
+        if(navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia(
+                {video : {width: 224, height: 224}}).then(stream => {
+                    webcamElement.srcObject = stream;
+                    webcamElement.addEventListener('loadeddata', async () => {
+                        adjustVideoSize(webcamElement.videoWidth, webcamElement.videoHeight);
+                        resolve();
+                    }, false);
+                }).catch(error => {
+                    reject(error);
+                });
+        } else {
+            reject();
+        }
+    });
+}
