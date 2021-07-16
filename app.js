104bc7a0f2fb0827a72a7a76fda6000349b33b70
ethanial1 <camiloeiz00@gmail.com>
2021-07-16 14:31:04 -0500

    CAPTURAMOS LAS TRAMAS

--- app.js
+++ app.js
@@ -48,3 +48,38 @@ async function cargarMobilenet() {
     const layer = mobileNetModel.getLayer('conv_pw_13_relu');
     mobilenet = tf.model({inputs: mobileNetModel.inputs, outputs: layer.output});
 }
+
+//=============== CAPTURAMOS LAS TRAMAS ============
+// Aquí necesitamos capturar imágenes sin procesar de la cámara web y preprocesarlas 
+// para su uso en nuestros modelos de aprendizaje profundo:
+
+function cropImage(img) {
+    const size = Math.min(img.shape[0], img.shape[1]);
+    // Encontramos el centro de la imagen
+    const centerHeight = img.shape[0] / 2;
+    const centerWidth = img.shape[1] / 2;
+
+    // Encontrar nuevos puntos de partida para la imagen recortada
+    const beginHeight = centerHeight - (size / 2);
+    const beginWidth = centerWidth - (size / 2);
+    return img.slice([beginHeight, beginWidth, 0],[size, size, 3]);
+}
+
+// Realiza las capturas
+function captura() {
+    // tf.tidy() ejecuta la función proporcionada y después 
+    // se ejecuta, limpia todos los tensores intermedios 
+    // asignados por esa función (excepto los devueltos)
+
+    return tf.tidy(() => {
+        // Crear un tf.Tensor a partir de una imagen
+        const webcamImage = tf.browser.fromPixels(webcamElement);
+        // Invertir la imagen horizontalmente
+        const revertirImagen = webcamImage.reverse(1);
+        // Recortar la imagen a un cuadrado con 3 canales (RGB)
+        const recortarImagen = cropImage(revertirImagen);
+        const lotesImagen = recortarImagen.expandDims(0);
+        // Normalizar la imagen de 0:255 a -1:1
+        return lotesImagen.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    });
+}
