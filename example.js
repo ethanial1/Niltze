c6ac9801b7a78d0d8e296618fe0dad1d67811145
ethanial1 <camiloeiz00@gmail.com>
2021-07-20 11:34:21 -0500

    Aplicando la técnica top down, creación de un método en concreto para cada acción.

--- example.js
+++ example.js
@@ -3,70 +3,79 @@ let net;
 const webcamElement = document.getElementById('webcam');
 const classifier = knnClassifier.create();
 
+let isPredicting = false;
+
+let webcam;
+
 // array del alfabeto
 let alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"];
 
 // =========== ==== INICIACIÓN DE LA CÁMARA Y EL MODELO MOBILENET ==== ==========
-// ajustamos el tamaño de la cámara
-function ajustarVideo(width, height) {
-    const aspectRatio = width / height;
-    if(width >= height){
-        webcamElement.width = aspectRatio * webcamElement.height;
-    }else if(width < height){
-        webcamElement.height = webcamElement.width / aspectRatio;
-    }
+// Inicializar la cámara
+async function cargarCamara() {
+    webcam = await tf.data.webcam(webcamElement);
+    console.log("Cámara cargada correctamente");
 }
 
 
 // Descargamos y preparamos el modelo MobileNet
 async function cargarModelo() {
     console.log('Cargando modelo...');
-}
-
-async function app() {
-    
-
     // Cargar el modelo
     net = await mobilenet.load();
     console.log('Modelo cargado correctamente.');
+}
 
-    const webcam = await tf.data.webcam(webcamElement);
+// =================== ==== Creación de un ejemplo de entrenamiento === =====================
+async function addEjemplo(id) {
+    // capturamos la imagen que sirve como ejemplo
+    const img = await webcam.capture();
 
-    const addExample = async classId => {
-        // capturar la imagen de la camara
-        const img = await webcam.capture();
+    //
+    const activation = net.infer(img, true);
 
-        const activation = net.infer(img, true);
+    // añadimos el ejemplo
+    classifier.addExample(activation, id);
 
-        classifier.addExample(activation, classId);
-        
-        img.dispose();
-    };
-
-    // When clicking a button, add an example for that class.
-    document.getElementById('class-a').addEventListener('click', () => addExample(0));
-    document.getElementById('class-b').addEventListener('click', () => addExample(1));
-    document.getElementById('class-c').addEventListener('click', () => addExample(2));
-
-    while (true) {
-        if (classifier.getNumClasses() > 0){
-            const img = await webcam.capture();
-
-            const activation = net.infer(img, 'conv_preds');
-
-            const result = await classifier.predictClass(activation);
-
-            const classes = ['A', 'B', 'C'];
-
-            document.getElementById('console').innerText = `prediction: ${classes[result.label]}\nprobability: ${result.confidences[result.label]}`;
-
-            // destruir el tensor 
-            img.dispose();
-        }
-        await tf.nextFrame();
-    }
-
+    // destruimos a la imagen
+    img.dispose();
 }
 
 
-app();
+
+// ============ === Proceso de predicciones === ===========
+async function predecir() {
+    while (isPredicting) {
+        // Captura la nueva imagen obtenida de la cámara
+        const img = await webcam.capture();
+        // activación
+        const activation = net.infer(img, 'conv_preds');
+        //
+        const result = await classifier.predictClass(activation);
+
+        document.getElementById('console').innerText = `prediction: ${alfabeto[result.label]}\nprobability: ${result.confidences[result.label]}`;
+
+        // Escribimos la respuesta
+        if(result.confidences[result.label] == 1){
+            document.getElementById('respuesta').innerText = `${alfabeto[result.label]}`;
+        }
+        
+        // 
+        img.dispose();
+
+        await tf.nextFrame();
+    }
+}
+
+function setPredicting(preg) {
+    isPredicting = preg;
+    predecir();
+}
+
+// =============== ==== Función auxiliar ==== ================
+async function init() {
+    await cargarModelo();
+    await cargarCamara();
+}
+
+init();
